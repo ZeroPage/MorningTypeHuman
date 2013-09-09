3c9e117da300b37d620265e294264bfc8fe091fa
Skywave <skywave@live.co.kr>
2013-09-09 14:50:45 +0900

    Added SettingActivity

---
+++ MornigTypeHumanProject/MornigTypeHuman/src/main/java/org/zeropage/gdg/morningTypeHuman/view/SettingActivity.java
@@ -0,0 +1,45 @@
+package org.zeropage.gdg.morningTypeHuman.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+
+import org.zeropage.gdg.morningTypeHuman.R;
+
+public class SettingActivity extends PreferenceActivity {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preference);
+
+        Preference statsPreference = findPreference("stats");
+        Preference creditsPreference = findPreference("credits");
+
+        if (statsPreference == null || creditsPreference == null) {
+            throw new AssertionError();
+        }
+
+        statsPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+            //Intent intent = new Intent(SettingActivity.this, StatsActivity.class);
+            //startActivity(intent);
+
+            return false;
+            }
+        });
+
+        creditsPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+            //Intent intent = new Intent(SettingActivity.this, CreditsActivity.class);
+            //startActivity(intent);
+
+            return false;
+            }
+        });
+    }
+}
