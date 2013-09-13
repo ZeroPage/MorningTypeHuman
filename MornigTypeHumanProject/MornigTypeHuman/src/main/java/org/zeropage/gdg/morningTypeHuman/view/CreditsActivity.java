f2b1415d0679d3335056a67cb3b2e151e94175d2
neropsys <neropsys@gmail.com>
2013-09-14 05:10:11 +0900

    Added Credits, Day of the Week Layouts

---
+++ MornigTypeHumanProject/MornigTypeHuman/src/main/java/org/zeropage/gdg/morningTypeHuman/view/CreditsActivity.java
@@ -0,0 +1,17 @@
+package org.zeropage.gdg.morningTypeHuman.view;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import org.zeropage.gdg.morningTypeHuman.R;
+
+public class CreditsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+
+    
+}
