0397ac9cf074bb6f5880d81f4a6a2e59eebc12b3
Ko HanJong <rino0601@naver.com>
2013-09-14 21:06:30 +0900

    implement Google+ Login

--- MornigTypeHumanProject/MornigTypeHuman/src/main/java/org/zeropage/gdg/morningTypeHuman/view/LaunchActivity.java
+++ MornigTypeHumanProject/MornigTypeHuman/src/main/java/org/zeropage/gdg/morningTypeHuman/view/LaunchActivity.java
@@ -1,12 +1,11 @@
 package org.zeropage.gdg.morningTypeHuman.view;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-
-
+import android.widget.Toast;
 
 import org.zeropage.gdg.morningTypeHuman.R;
+import org.zeropage.gdg.morningTypeHuman.basegameutils.BaseGameActivity;
 import org.zeropage.gdg.morningTypeHuman.controller.LaunchActivityController;
 import org.zeropage.gdg.morningTypeHuman.model.AppStatisticsManager;
 import org.zeropage.gdg.morningTypeHuman.model.FileStorage;
@@ -14,7 +13,7 @@ import org.zeropage.gdg.morningTypeHuman.model.FileStorage;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class LaunchActivity extends Activity {
+public class LaunchActivity extends BaseGameActivity {
     protected int _splashTime = 2000;
 
     @Override
@@ -25,19 +24,10 @@ public class LaunchActivity extends Activity {
         // init here.
         FileStorage.init(this);
         AppStatisticsManager.init(this);
-
         AppStatisticsManager.increaseTotalLaunched(this);
 
         LaunchActivityController controller = new LaunchActivityController(this);
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                finish();
-                startActivity(new Intent(LaunchActivity.this, MainActivity.class));
-            }
-        };
-        Timer timer = new Timer();
-        timer.schedule(task, _splashTime);
+
     }
 
     @Override
@@ -46,5 +36,28 @@ public class LaunchActivity extends Activity {
         finish();
     }
 
+    @Override
+    public void onSignInFailed() {
+        setAction();
+        Toast.makeText(this,"로그인 실패! 본 앱은 Google+ 로그인이 필요합니다.", Toast.LENGTH_LONG).show();
+    }
 
+    private void setAction() {
+        if(isSignedIn()) {
+            TimerTask loginTask = new TimerTask() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+                }
+            };
+            new Timer().schedule(loginTask, _splashTime);
+        } else {
+            beginUserInitiatedSignIn();
+        }
+    }
+
+    @Override
+    public void onSignInSucceeded() {
+        setAction();
+    }
 }
