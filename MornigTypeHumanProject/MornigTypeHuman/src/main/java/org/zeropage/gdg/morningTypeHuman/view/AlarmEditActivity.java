eb79f06a0631a2e2e9d07aedc0498897fa2889eb
Ko HanJong <rino0601@naver.com>
2013-09-14 17:32:35 +0900

    keyboard problem fixed.

--- MornigTypeHumanProject/MornigTypeHuman/src/main/java/org/zeropage/gdg/morningTypeHuman/view/AlarmEditActivity.java
+++ MornigTypeHumanProject/MornigTypeHuman/src/main/java/org/zeropage/gdg/morningTypeHuman/view/AlarmEditActivity.java
@@ -2,6 +2,8 @@ package org.zeropage.gdg.morningTypeHuman.view;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 import org.zeropage.gdg.morningTypeHuman.R;
 import org.zeropage.gdg.morningTypeHuman.controller.AlarmEditActivityController;
@@ -14,6 +16,9 @@ public class AlarmEditActivity extends FragmentActivity {
         setContentView(R.layout.activity_alarm_edit);
 
         AlarmEditActivityController controller = new AlarmEditActivityController(this);
+
+        EditText editText = (EditText) findViewById(R.id.editTextLectureName);
+        editText.setImeOptions(EditorInfo.IME_ACTION_DONE);
 
         controller.update();
     }
